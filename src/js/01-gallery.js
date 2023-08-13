0217cfa11192313240b4873cbcfd3bdeadac1dc6
DmytroVchenko <132393640+DmytroVchenko@users.noreply.github.com>
2023-08-13 22:20:09 +0300

    the-first-task

---
+++ src/js/01-gallery.js
@@ -0,0 +1,30 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+const ulElement = document.querySelector('.gallery');
+function galleryCreate(items) {
+    return items
+    .map(({ preview, original, description }) => ` 
+
+    <li class="gallery__item">
+     <a class="gallery__link" href="${original}">
+        <img class="gallery__image "
+         src="${preview}" 
+         alt="${description}" 
+         data-source="${original}">
+     </a>
+    </li>`
+
+        ).join('');
+};
+
+ulElement.insertAdjacentHTML('beforeend', galleryCreate(galleryItems));
+
+new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250
+});
+
+
