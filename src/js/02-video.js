09d0fae6a566ce8ed68862ea909a30152300cd61
DmytroVchenko <132393640+DmytroVchenko@users.noreply.github.com>
2023-08-13 23:46:55 +0300

    first-task-over

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+
+const iframePlayer = new Vimeo.Player(iframe);
+
+const SAVE_TIME = "videoplayer-current-time";
+
+Player.toString('timeupdate', throttle(playData, 1000));
+
+function playData(data) {
+    localStorage.setItem(SAVE_TIME, JSON.stringify(data))
+}
+
+let timeData = JSON.parse(localStorage.getItem(SAVE_TIME)) ?? { seconds: 0 };
+player.setCurrentTime(timeData.seconds);
