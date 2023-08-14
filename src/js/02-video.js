4aef66e321265b933d2401b3c1b7fce78abc3847
DmytroVchenko <132393640+DmytroVchenko@users.noreply.github.com>
2023-08-14 14:47:03 +0300

    second-task-done
    alpha version

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,20 @@
-import Player from '@vimeo/player';
 
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
-
-const iframePlayer = new Vimeo.Player(iframe);
-
 const SAVE_TIME = "videoplayer-current-time";
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
-Player.toString('timeupdate', throttle(playData, 1000));
+document.addEventListener('click', callBack);
 
-function playData(data) {
-    localStorage.setItem(SAVE_TIME, JSON.stringify(data))
-}
+function callBack() {
+   player.on('timeupdate', throttle(onPlay, 1000));
+    function onPlay(data) {
+        const currentTime = data.seconds;
+        localStorage.setItem(SAVE_TIME, JSON.stringify({ seconds: currentTime }));
+    }
+};
 
 let timeData = JSON.parse(localStorage.getItem(SAVE_TIME)) ?? { seconds: 0 };
-player.setCurrentTime(timeData.seconds);
+    player.setCurrentTime(timeData.seconds);
